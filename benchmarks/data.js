d5a6599d65831447a4a7ee4f5932bef256ab51e2
github-action-benchmark <github@users.noreply.github.com>
2026-03-24 15:24:44 +0800

    add salt-benchmark-throughput (customBiggerIsBetter) benchmark result for f47207b28edb0285283d5d5de2ca57198c253c02

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774337080938,
+  "lastUpdate": 1774337084374,
   "repoUrl": "https://github.com/megaeth-labs/salt",
   "entries": {
     "salt-benchmark-time": [
@@ -49,6 +49,57 @@ window.BENCHMARK_DATA = {
             "name": "update 10000 KVs/16 threads",
             "value": 24.946,
             "unit": "ms"
+          }
+        ]
+      }
+    ],
+    "salt-benchmark-throughput": [
+      {
+        "commit": {
+          "author": {
+            "email": "yunlong@megaeth.technology",
+            "name": "yunlonggao-mega",
+            "username": "yunlonggao-mega"
+          },
+          "committer": {
+            "email": "yunlong@megaeth.technology",
+            "name": "yunlonggao-mega",
+            "username": "yunlonggao-mega"
+          },
+          "distinct": true,
+          "id": "f47207b28edb0285283d5d5de2ca57198c253c02",
+          "message": "use self hosted runner to run perf benchmark",
+          "timestamp": "2026-03-24T15:19:06+08:00",
+          "tree_id": "8acccc0f36ecdc569e55c6e931f1cde37c8bd4a6",
+          "url": "https://github.com/megaeth-labs/salt/commit/f47207b28edb0285283d5d5de2ca57198c253c02"
+        },
+        "date": 1774337082668,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "update 10000 KVs/1 threads",
+            "value": 49023,
+            "unit": "elem/s"
+          },
+          {
+            "name": "update 10000 KVs/2 threads",
+            "value": 89558,
+            "unit": "elem/s"
+          },
+          {
+            "name": "update 10000 KVs/4 threads",
+            "value": 159570,
+            "unit": "elem/s"
+          },
+          {
+            "name": "update 10000 KVs/8 threads",
+            "value": 274620,
+            "unit": "elem/s"
+          },
+          {
+            "name": "update 10000 KVs/16 threads",
+            "value": 400860,
+            "unit": "elem/s"
           }
         ]
       }
